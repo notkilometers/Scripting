d6cb29eda34a9abb60f155ba3324e5f41c4d9ce5
notkilometers <47023811+notkilometers@users.noreply.github.com>
2020-09-25 17:14:11 -0700

    added files

---
+++ Active-Adapter-Restart.ps1
@@ -0,0 +1,10 @@
+﻿$t = Get-NetAdapter | Out-String
+#$p = $t.Split([Environment]::NewLine)
+$p = $t.Split("`r`n")
+$len = $p.Count
+For ($i=0; $i-le $len; $i++) {
+    if ($p[$i] -Match "Up") {
+        $q = $p[$i].Split(" ")
+    }
+}
+Restart-NetAdapter -Name $q[0]
